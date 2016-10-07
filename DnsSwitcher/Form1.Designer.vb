2f7ce2e15de74de72f8203fbd38b5eefc15e60b9
augus <augus@AUGUST-PC>
2016-10-07 19:25:26 -0400

    Add project files.

---
+++ DnsSwitcher/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.Options = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Options, Me.ToolStripButton1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(644, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'Options
+        '
+        Me.Options.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.Options.Image = CType(resources.GetObject("Options.Image"), System.Drawing.Image)
+        Me.Options.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.Options.Name = "Options"
+        Me.Options.Size = New System.Drawing.Size(42, 22)
+        Me.Options.Text = "Menu"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(44, 22)
+        Me.ToolStripButton1.Text = "About"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 184)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "Form1"
+        Me.Text = "DnsSwitcher"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents Options As ToolStripButton
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+End Class
---
+++ DnsSwitcher/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System
+Imports System.Security
+Imports System.Security.Cryptography
+Imports System.Text
+Imports Ionic.Zip
+
+Public Class Form1
+    Dim QuicksetDnsexe As String
+    Dim projectDirectory As String
+    Dim QuicksetDnszip As String
+    Dim QuicksetDnsdir As String
+
+
+
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DownloadQuickDns()
+    End Sub
+
+    Private Sub DownloadQuickDns()
+        projectDirectory = My.Computer.FileSystem.CurrentDirectory
+        QuicksetDnsexe = projectDirectory
+        QuicksetDnsexe += "/QuicksetDns/QuickSetDNS.exe"
+        QuicksetDnszip = projectDirectory
+        QuicksetDnszip += "/quicksetdns.zip"
+        QuicksetDnsdir = projectDirectory
+        QuicksetDnsdir += "/QuicksetDns/"
+
+        If My.Computer.FileSystem.FileExists(QuicksetDnsexe) Then
+            GoTo quit
+        Else
+            MsgBox("QuicksetDns not downloaded or Broken, Downloading now")
+            My.Computer.Network.DownloadFile("http://www.nirsoft.net/utils/quicksetdns.zip", QuicksetDnszip)
+            Console.WriteLine("Extracting file {0} to {1}", QuicksetDnszip, QuicksetDnsdir)
+            Using zip1 As ZipFile = ZipFile.Read(QuicksetDnszip)
+                ' AddHandler zip1.ExtractProgress, AddressOf MyExtractProgress
+                Dim e As ZipEntry
+                ' here, we extract every entry, but we could extract    
+                ' based on entry name, size, date, etc.   
+                For Each e In zip1
+                    e.Extract(QuicksetDnsdir, ExtractExistingFileAction.OverwriteSilently)
+                Next
+            End Using
+            My.Computer.FileSystem.DeleteFile(QuicksetDnszip)
+        End If
+quit:
+    End Sub
+
+End Class
